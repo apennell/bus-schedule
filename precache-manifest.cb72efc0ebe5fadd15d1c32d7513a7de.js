c4434b3f9883894c98e077d4558ff8c459f30f51
Annie Pennell <anniecpennell@gmail.com>
2020-02-09 22:49:00 -0800

    Updates

---
+++ precache-manifest.cb72efc0ebe5fadd15d1c32d7513a7de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9ead333b6ba247ba7b4205409df331",
+    "url": "/bus-schedule/index.html"
+  },
+  {
+    "revision": "13d425cc337c5521d77d",
+    "url": "/bus-schedule/static/css/main.09a7e56a.chunk.css"
+  },
+  {
+    "revision": "c2c9ed152a4c5cc82f14",
+    "url": "/bus-schedule/static/js/2.ea505877.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bus-schedule/static/js/2.ea505877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d425cc337c5521d77d",
+    "url": "/bus-schedule/static/js/main.e4ea6679.chunk.js"
+  },
+  {
+    "revision": "87f10129cdf7f6a42090",
+    "url": "/bus-schedule/static/js/runtime-main.1e400d21.js"
+  }
+]);
